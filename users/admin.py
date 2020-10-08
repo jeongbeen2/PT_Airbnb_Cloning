10bcc4992e6fc258b0c56ed80a1d99ee098f2cd0
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-10-08 17:14:54 +0900

    user panel

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,33 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
-@admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
+@admin.register(models.User)  # Decorator
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
     add_fieldsets = (
         (
             "NewBox",
             {
-                "fields": ("Bio",),
+                "fields": (
+                    "Bio",
+                    "avatar",
+                    "gender",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                ),
             },
         ),
     )
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = ("superhost",)
+
+
+# admin.site.register(models.User, UserAdmin)
+# @admin.register(models.User) 와 똑같음.

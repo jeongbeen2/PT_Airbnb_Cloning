065d3119bf8612e043192758d0359f67ab3a76f2
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-10-10 17:54:34 +0900

    #5.0 Review model done

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+    """ Review Model Definitions """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        """ ForeignKey를 정의하면, 정의한 값 내에있는 값도 가져올 수 있음. """
+        """ ex) self.room.city """
+        """ ex2) self.room.host.username """
+        """ 만일, 부르려는 값의 __str__값이 정의되어있으면 그값을 가져온다. """
+        return f"{self.user} says : {self.review}"

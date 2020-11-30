ddd3704e629097bb5c2ece01742754b2469c2b41
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-11-30 16:03:16 +0900

    #9.5 seed_room done!

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -38,14 +38,34 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(0, 5),
             },
         )
+        """ #9.4 >> flatten은, [[13]] 과 같은 형식을 [13]으로 바꿔준다. """
+        """ 만들어진 방의 넘버를 primary key, pk로 지정하고, 그 방안에 3~17개의 사진을 넣어주는 과정임. """
         created_photos = seeder.execute()
         created_clean = flatten(list(created_photos.values()))
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
-            for i in range(3, random.randint(10, 17)):
+            for i in range(3, random.randint(10, 30)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f"/room_photos/{random.randint(1,31)}.webp",
                 )
+            """ #9.5 >> Many To Many Field에서 새로운 것을 추가할 때, 0~15의 숫자를 뽑아 그중 2로 나누어진다면 추가하는 방법. """
+            """ 많이 쓰는 방식이다. """
+            for a in amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenities.add(a)
+            for f in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facilities.add(f)
+            for r in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
+
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))

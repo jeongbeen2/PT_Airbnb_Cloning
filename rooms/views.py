145470d54f00aa966017966670b707ce2a3a76cc
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-01 13:38:17 +0900

    #11.1 request.GET.get()

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from . import models
 
+
 """ #10.1 >> request는, view 함수의 첫번째 인자다. """
 """ request가 있어야지만, response가 있다. """
 
+""" #11.1 >> request.GET에는 많은 메소드가 들어있고, 그중에 get이라는 메소드를 이용하면 url의 일부를 key, value값으로 가져올수 있다. """
+""" 또한, get으로 가져오는 것은 str이기 때문에, int로 바꿔 주어야함. """
+
 
 def all_rooms(request):
-    """ django에게, all_rooms.html을 comfile하라고 명령하는 것이다. """
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(
         request,
         "rooms/home.html",

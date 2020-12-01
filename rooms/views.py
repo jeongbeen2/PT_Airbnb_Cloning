0925b613edadf692f0ab53d50d96e5bfbd4aead8
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-01 14:08:35 +0900

    #11.3 manual set without Django

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -7,18 +8,30 @@ from . import models
 
 """ #11.1 >> request.GET에는 많은 메소드가 들어있고, 그중에 get이라는 메소드를 이용하면 url의 일부를 key, value값으로 가져올수 있다. """
 """ 또한, get으로 가져오는 것은 str이기 때문에, int로 바꿔 주어야함. """
+""" #11.2 >> 빈 페이지로 갈 경우, 기본값 1을 제공한다. """
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
+
+    page_count = ceil(
+        models.Room.objects.count() / page_size
+    )  # 이 경우, Room.object의 모든 갯수를 count 해주는 메소드이다.
+
     all_rooms = models.Room.objects.all()[offset:limit]
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": all_rooms},
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
     )
 
 

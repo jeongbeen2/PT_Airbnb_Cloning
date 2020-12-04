73c7d6f6536dceac444d1e8176a3b2bb4ccb5ee0
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-04 19:46:10 +0900

    #13.1 making form without django magic

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import redirect, render
-
 # from django.core.paginator import EmptyPage, Paginator
 # from django.http import Http404
 from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect, render
+from django_countries import countries
 from . import models
 
 
@@ -40,9 +40,15 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    print(request.GET)
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
 
 
 """ #12.4 >> RoomDetail을 만들면, model이라는 QuerySet을 가져오고 Django에서 정해준 이름인 detail.html -> room_detail.html로 수정해야함. """

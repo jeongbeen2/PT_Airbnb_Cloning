d0d0c5f5a311a5faf281eed988f61582558da0e0
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-09 01:05:26 +0900

    #14.3 validating email & password done

--- users/forms.py
+++ users/forms.py
@@ -11,22 +11,18 @@ class LoginForm(forms.Form):
 
     """ #14.2 >> 이메일이나, 비밀번호가 있는 field를 확인하고 싶으면 clean_ 을 붙여야한다. """
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-
-    def clean_password(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
         try:
-            user = models.User.objects.get(username=email)
-            if user.check_password(password):
-                return password
+            user = models.User.objects.get(email=email)  # email이 실제 유저의 이메일과 같은지 체크
+            if user.check_password(password):  # password 유효한지 체크
+                return self.cleaned_data
+                # clean을 사용했다면, 항상 return값은 cleaned_data로 해주어야한다.
             else:
-                raise forms.ValidationError("Password is wrong")
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            pass
+            self.add_error("email", forms.ValidationError("USer does not exist"))
+
+    """ #14.3 >> def clean으로 할거면, 에러가 뜨는곳을 직접 add_error 해줘서 넣어주어야 하고, """
+    """ def clean_email, password처럼 각각 해줄꺼면 raise password, email 해주면 그자리에 뜬다. """
--- users/views.py
+++ users/views.py
@@ -10,12 +10,6 @@ class LoginView(View):
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-
-        """ #14.2 >> cleaned_data = 정리된 데이터라는 뜻으로, form.cleaned_data는 입력된 data를 dict형태로 정리해준다. """
-        """ if form.isvalid()는, 입력값이 맞는지 판단하는 메소드이다. """
-        """ 이때, forms에서 clean_ method를 사용하면, 받아지는 데이터를 내맘대로 바꿀수있음. """
-        """     def clean_email(self):
-                    print("clean email") """
         if form.is_valid():
             print(form.cleaned_data)
 

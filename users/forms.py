738efd158c78f852bbae7bf83341fa0c5703a9b5
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-09 00:50:39 +0900

    #14.2 clean_email, validating this

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 """ #14.1 >> form을 작성하고, 그다음 views로 넘어가서 form을 넣어준다. """
 
@@ -7,3 +8,16 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    """ #14.2 >> 이메일이나, 비밀번호가 있는 field를 확인하고 싶으면 clean_ 을 붙여야한다. """
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        print("clean password")
--- users/views.py
+++ users/views.py
@@ -5,9 +5,18 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "hello@nico.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+
+        """ #14.2 >> cleaned_data = 정리된 데이터라는 뜻으로, form.cleaned_data는 입력된 data를 dict형태로 정리해준다. """
+        """ if form.isvalid()는, 입력값이 맞는지 판단하는 메소드이다. """
+        """ 이때, forms에서 clean_ method를 사용하면, 받아지는 데이터를 내맘대로 바꿀수있음. """
+        """     def clean_email(self):
+                    print("clean email") """
+        if form.is_valid():
+            print(form.cleaned_data)
+
+        return render(request, "users/login.html", {"form": form})

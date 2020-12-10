7bab4873e32c4e9f1a8e23f1e1de9d9f0dbc2e49
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-10 18:10:13 +0900

    #14.5 wip

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
@@ -24,3 +26,12 @@ class LoginView(View):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+""" #14.5 >> LoginView에 대해 더 자세히 알고싶다면 """
+""" https://docs.djangoproject.com/en/2.2/topics/auth/default/ """
+
+""" FormView는, form에서 인증하고싶을때 좋다. """
+""" ccbv에서 확인. """
+
+""" reverse는, core:home으로 가서 실제 url을 가져다준다. """

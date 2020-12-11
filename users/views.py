ada417e0e57f8233b59bb401868cbf778cf3ad31
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-11 11:49:37 +0900

    #15.1 Signup Form part one

--- users/views.py
+++ users/views.py
@@ -34,3 +34,9 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "Nicolas",
+        "last_name": "Serr",
+        "email": "itn@las.com",
+    }

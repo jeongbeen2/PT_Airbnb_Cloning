9ed420c28d40c39b3e04e09cdfec9dc0bbd4a204
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-12-20 23:42:03 +0900

    #20.7 Room Card done!

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,16 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
+    },
   },
   variants: {},
   plugins: [],
 }
 
+/* #20.4 >> "25vh": "25vh"에서, 왼쪽은 class name, 오른쪽은 css 항목이다. */
